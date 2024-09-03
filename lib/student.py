b27791ab9cc93943759a7d459ee00b52b5bca3ef
limoh653 <limohesbon7@gmail.com>
2024-09-03 17:42:38 +0300

    some tests not passsing

--- lib/student.py
+++ lib/student.py
@@ -1,5 +1,32 @@
-class Student:
-    pass
+#!/usr/bin/env python
+
+from user import User
+
+class Student(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []
+
+    def learn(self, new_knowledge):
+        self.knowledge.append(new_knowledge)
+
+    def hello(self):
+        print("Hey there! I'm so excited to learn stuff.")
+
+    def raise_hand(self):
+        print("Pick me!")
+
 
 class ChattyStudent(Student):
-    pass
+    def hello(self):
+        # Use super() to inherit the parent class's hello() method
+        super().hello()
+        # Add additional "chatty" behavior
+        print("How are you doing today? I'm okay, but I'm kind of tired.")
+        print("Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy!")
+        print("What, you don't want any spoilers? Okay well let me just tell you who died...")
+
+    def raise_hand(self):
+        # Call super() multiple times to print "Pick me!" ten times
+        for _ in range(10):
+            super().raise_hand()
